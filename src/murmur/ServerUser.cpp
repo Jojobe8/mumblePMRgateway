4f16575b61ffab49ff5fcc6fcbb27a392f84f6f7
jojobe8 <jojo.benker@gmx.de>
2020-04-08 18:54:46 +0200

    Basecode from https://github.com/mumble-voip/mumble/releases/download/1.3.0-rc2/mumble-1.3.0-rc2.zip

---
+++ src/murmur/ServerUser.cpp
@@ -0,0 +1,154 @@
+// Copyright 2005-2019 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "murmur_pch.h"
+
+#include "Server.h"
+#include "ServerUser.h"
+#include "Meta.h"
+
+ServerUser::ServerUser(Server *p, QSslSocket *socket) : Connection(p, socket), User(), s(NULL), leakyBucket(p->iMessageLimit, p->iMessageBurst) {
+	sState = ServerUser::Connected;
+	sUdpSocket = INVALID_SOCKET;
+
+	memset(&saiUdpAddress, 0, sizeof(saiUdpAddress));
+	memset(&saiTcpLocalAddress, 0, sizeof(saiTcpLocalAddress));
+
+	dUDPPingAvg = dUDPPingVar = 0.0f;
+	dTCPPingAvg = dTCPPingVar = 0.0f;
+	uiUDPPackets = uiTCPPackets = 0;
+
+	aiUdpFlag = 1;
+	uiVersion = 0;
+	bVerified = true;
+	iLastPermissionCheck = -1;
+	
+	bOpus = false;
+}
+
+
+ServerUser::operator QString() const {
+	return QString::fromLatin1("%1:%2(%3)").arg(qsName).arg(uiSession).arg(iId);
+}
+BandwidthRecord::BandwidthRecord() {
+	iRecNum = 0;
+	iSum = 0;
+	for (int i=0;i<N_BANDWIDTH_SLOTS;i++)
+		a_iBW[i] = 0;
+}
+
+bool BandwidthRecord::addFrame(int size, int maxpersec) {
+	QMutexLocker ml(&qmMutex);
+
+	quint64 elapsed = a_qtWhen[iRecNum].elapsed();
+
+	if (elapsed == 0)
+		return false;
+
+	int nsum = iSum-a_iBW[iRecNum]+size;
+	int bw = static_cast<int>((nsum * 1000000LL) / elapsed);
+
+	if (bw > maxpersec)
+		return false;
+
+	a_iBW[iRecNum] = static_cast<unsigned short>(size);
+	a_qtWhen[iRecNum].restart();
+
+	iSum = nsum;
+
+	iRecNum++;
+	if (iRecNum == N_BANDWIDTH_SLOTS)
+		iRecNum = 0;
+
+	return true;
+}
+
+int BandwidthRecord::onlineSeconds() const {
+	QMutexLocker ml(&qmMutex);
+
+	return static_cast<int>(tFirst.elapsed() / 1000000LL);
+}
+
+int BandwidthRecord::idleSeconds() const {
+	QMutexLocker ml(&qmMutex);
+
+	quint64 iIdle = a_qtWhen[(iRecNum + N_BANDWIDTH_SLOTS - 1) % N_BANDWIDTH_SLOTS].elapsed();
+	if (tIdleControl.elapsed() < iIdle)
+		iIdle = tIdleControl.elapsed();
+
+	return static_cast<int>(iIdle / 1000000LL);
+}
+
+void BandwidthRecord::resetIdleSeconds() {
+	QMutexLocker ml(&qmMutex);
+
+	tIdleControl.restart();
+}
+
+int BandwidthRecord::bandwidth() const {
+	QMutexLocker ml(&qmMutex);
+
+	int sum = 0;
+	int records = 0;
+	quint64 elapsed = 0ULL;
+
+	for (int i=1;i<N_BANDWIDTH_SLOTS;++i) {
+		int idx = (iRecNum + N_BANDWIDTH_SLOTS - i) % N_BANDWIDTH_SLOTS;
+		quint64 e = a_qtWhen[idx].elapsed();
+		if (e > 1000000ULL) {
+			break;
+		} else {
+			++records;
+			sum += a_iBW[idx];
+			elapsed = e;
+		}
+	}
+
+	if (elapsed < 250000ULL)
+		return 0;
+
+	return static_cast<int>((sum * 1000000ULL) / elapsed);
+}
+
+inline static QTime now() {
+	return QTime::currentTime();
+}
+
+inline static int millisecondsBetween(const QTime &start, const QTime &end) {
+	return start.msecsTo(end);
+}
+
+// Rate limiting: burst up to 5, 1 message per sec limit over longer time
+LeakyBucket::LeakyBucket(unsigned int tokensPerSec, unsigned int maxTokens) : tokensPerSec(tokensPerSec), maxTokens(maxTokens), currentTokens(0) {
+	lastUpdate = now();
+}
+
+bool LeakyBucket::ratelimit(int tokens) {
+	// First remove tokens we leaked over time
+	const QTime tnow = now();
+	const long ms = millisecondsBetween(lastUpdate, tnow);
+
+	const long drainTokens = (ms * tokensPerSec) / 1000;
+
+	// Prevent constant starvation due to too many updates
+	if (drainTokens > 0) {
+		this->lastUpdate = tnow;
+
+		this->currentTokens -= drainTokens;
+		if (this->currentTokens < 0) {
+			this->currentTokens = 0;
+		}
+	}
+
+	// Then try to add tokens
+	bool limit = this->currentTokens > ((static_cast<long>(maxTokens)) - tokens);
+
+	// If the bucket is not overflowed, allow message and add tokens
+	if (!limit) {
+		this->currentTokens += tokens;
+	}
+
+	return limit;
+}
---
+++ src/murmur/ServerUser.h
@@ -0,0 +1,136 @@
+// Copyright 2005-2019 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MURMUR_SERVERUSER_H_
+#define MUMBLE_MURMUR_SERVERUSER_H_
+
+#include <QtCore/QStringList>
+#include <QtCore/QTime>
+
+#ifdef Q_OS_UNIX
+#include <sys/socket.h>
+#else
+#include <winsock2.h>
+#endif
+
+#include "Connection.h"
+#include "Timer.h"
+#include "User.h"
+#include "HostAddress.h"
+
+// Unfortunately, this needs to be "large enough" to hold
+// enough frames to account for both short-term and
+// long-term "maladjustments".
+
+#define N_BANDWIDTH_SLOTS 360
+
+struct BandwidthRecord {
+	int iRecNum;
+	int iSum;
+	Timer tFirst;
+	Timer tIdleControl;
+	unsigned short a_iBW[N_BANDWIDTH_SLOTS];
+	Timer a_qtWhen[N_BANDWIDTH_SLOTS];
+	mutable QMutex qmMutex;
+
+	BandwidthRecord();
+	bool addFrame(int size, int maxpersec);
+	int onlineSeconds() const;
+	int idleSeconds() const;
+	void resetIdleSeconds();
+	int bandwidth() const;
+};
+
+struct WhisperTarget {
+	struct Channel {
+		int iId;
+		bool bChildren;
+		bool bLinks;
+		QString qsGroup;
+	};
+	QList<unsigned int> qlSessions;
+	QList<WhisperTarget::Channel> qlChannels;
+};
+
+class Server;
+
+// Simple algorithm for rate limiting
+class LeakyBucket {
+	private:
+		unsigned int tokensPerSec, maxTokens;
+		long currentTokens;
+		QTime lastUpdate;
+
+	public:
+		// Returns true if packets should be dropped
+		bool ratelimit(int tokens);
+
+		LeakyBucket(unsigned int tokensPerSec, unsigned int maxTokens);
+};
+
+class ServerUser : public Connection, public User {
+	private:
+		Q_OBJECT
+		Q_DISABLE_COPY(ServerUser)
+	protected:
+		Server *s;
+	public:
+		enum State { Connected, Authenticated };
+		State sState;
+		operator QString() const;
+
+		float dUDPPingAvg, dUDPPingVar;
+		float dTCPPingAvg, dTCPPingVar;
+		quint32 uiUDPPackets, uiTCPPackets;
+
+		unsigned int uiVersion;
+		QString qsRelease;
+		QString qsOS;
+		QString qsOSVersion;
+
+		std::string ssContext;
+		QString qsIdentity;
+
+		bool bVerified;
+		QStringList qslEmail;
+
+		HostAddress haAddress;
+
+		/// Holds whether the user is using TCP
+		/// or UDP for voice packets.
+		///
+		/// If the flag is 0, the user is using
+		/// TCP.
+		///
+		/// If the flag is 1, the user is using
+		/// UDP.
+		QAtomicInt aiUdpFlag;
+
+		QList<int> qlCodecs;
+		bool bOpus;
+
+		QStringList qslAccessTokens;
+
+		QMap<int, WhisperTarget> qmTargets;
+		typedef QPair<QSet<ServerUser *>, QSet<ServerUser *> > TargetCache;
+		QMap<int, TargetCache> qmTargetCache;
+		QMap<QString, QString> qmWhisperRedirect;
+
+		LeakyBucket leakyBucket;
+
+		int iLastPermissionCheck;
+		QMap<int, unsigned int> qmPermissionSent;
+#ifdef Q_OS_UNIX
+		int sUdpSocket;
+#else
+		SOCKET sUdpSocket;
+#endif
+		BandwidthRecord bwr;
+		struct sockaddr_storage saiUdpAddress;
+		struct sockaddr_storage saiTcpLocalAddress;
+		ServerUser(Server *parent, QSslSocket *socket);
+};
+
+#endif
